9b16ca808ad9fbf2ce9bba3f94e54b50c1add5d4
Jrigada <jrigada@est.frba.utn.edu.ar>
2023-12-04 15:42:54 -0300

    remove debug log

--- e2e-tests/contracts/TestCheatcodes.sol
+++ e2e-tests/contracts/TestCheatcodes.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.0;
 
 contract TestCheatcodes {
-  event Log(bytes32 message);
   address constant CHEATCODE_ADDRESS = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
 
   function testAddr(uint256 privateKey, address addr) external {
